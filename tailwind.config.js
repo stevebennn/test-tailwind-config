683cbcee4567b5bd17d5b2376c13a3eb976e8710
Stephen Bennett <s.bennett87@gmail.com>
2020-06-03 09:20:43 -0400

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,45 @@
+module.exports = {
+  theme: {
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      black: "#000",
+      white: "#fff",
+      primary: {
+        lighter: "#2d3748",
+        default: "#1a202c",
+        dark: "#000"
+      },
+      secondary: {
+        lighter: "#f6e05e",
+        default: "#ecc94b",
+        dark: "#d69e2e"
+      },
+      gray: {
+        100: "#f7fafc",
+        200: "#edf2f7",
+        300: "#e2e8f0",
+        400: "#cbd5e0",
+        500: "#a0aec0",
+        600: "#718096",
+        700: "#4a5568",
+        800: "#2d3748",
+        900: "#1a202c"
+      },
+      yellow: {
+        100: "#fffff0",
+        200: "#fefcbf",
+        300: "#faf089",
+        400: "#f6e05e",
+        500: "#ecc94b",
+        600: "#d69e2e",
+        700: "#b7791f",
+        800: "#975a16",
+        900: "#744210"
+      }
+    },
+    extend: {}
+  },
+  variants: {},
+  plugins: []
+};
